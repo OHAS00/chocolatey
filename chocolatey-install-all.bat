c820eb50263c98c5a0a45a2c98bcccac07b2d63f
OHAS00 <alone.hide.and.seek@gmail.com>
2015-04-09 20:54:50 +0900

    Create chocolatey-install-all.bat

---
+++ chocolatey-install-all.bat
@@ -0,0 +1,9 @@
+powershell Set-ExecutionPolicy RemoteSigned
+powershell iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+powershell Set-ExecutionPolicy Restricted
+ 
+set PATH=%PATH%;C:\Chocolatey\bin;
+ 
+call cinst .\packages.config
+ 
+pause
